91c5abfe753017ad69bc5a89ad6a1db21ab9fe96
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-10-09 13:58:46 +0200

    CreateBooking works

--- WebVaskEnTid/Pages/CreateBooking.cshtml.cs
+++ WebVaskEnTid/Pages/CreateBooking.cshtml.cs
@@ -11,13 +11,21 @@ namespace WebVaskEnTid.Pages
         private BookingService _bookingService = new BookingService();
         //private int[] _allowedHours = new int[2] { 6, 21 }
 
+
         [BindProperty]
-        internal Booking NewBooking { get; set; }
+        public string PhoneNumber { get; set; }
+        [BindProperty]
+        public DateOnly BookingDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        [BindProperty]
+        public TimeOnly BookingTime { get; set; }
+        [BindProperty]
+        public int MachineID { get; set; }
+        
 
         public CreateBookingModel(BookingService bookingService)
         {
             _bookingService = bookingService;
-            NewBooking = new Booking(1, 1, "test", DateOnly.FromDateTime(DateTime.Now), new TimeOnly(6,0,0));
+            //NewBooking = new Booking(1, 1, "test", DateOnly.FromDateTime(DateTime.Now), new TimeOnly(6,0,0));
         }
 
         public void OnGet()
@@ -27,12 +35,12 @@ namespace WebVaskEnTid.Pages
 
         public IActionResult OnPost()
         {
-            Debug.WriteLine($"CreateBooking OnPost();   PhoneNumber: {NewBooking.PhoneNumber}");
-            if (NewBooking.BookingTime.Hour >= 6 || NewBooking.BookingTime.Hour <= 21)
+            Debug.WriteLine($"CreateBooking OnPost();   PhoneNumber: {PhoneNumber}");
+            if (BookingTime.Hour >= 6 || BookingTime.Hour <= 21)
             {
-                _bookingService.CreateBooking(NewBooking);
+                _bookingService.CreateBooking(MachineID, PhoneNumber, BookingDate, BookingTime);
             }
-            else { Debug.WriteLine($"BookingTime out of bounds: {NewBooking.BookingTime}"); }
+            else { Debug.WriteLine($"BookingTime out of bounds: {BookingTime}"); }
 
             return RedirectToPage("/Index");
         }
