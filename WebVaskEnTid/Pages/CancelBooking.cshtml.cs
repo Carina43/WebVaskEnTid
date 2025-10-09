38816d9ff5565f0874710b39d0da375fce169b9a
Carina43 <cag003@edu.zealand.dk>
2025-10-09 11:28:23 +0200

    razor og cancelbooking

--- WebVaskEnTid/Pages/CancelBooking.cshtml.cs
+++ WebVaskEnTid/Pages/CancelBooking.cshtml.cs
@@ -1,10 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using VaskEnTid_Library.Service;
+using VaskEnTidLibrary.Model;
 
 namespace WebVaskEnTid.Pages
 {
     public class CancelBookingModel : PageModel
     {
+
+        private readonly BookingService _bookingService;
+
+        [BindProperty]
+        public List<Booking> Bookings { get; set; }
+
+        public CancelBookingModel(BookingService bookingService)
+        {
+            _bookingService = bookingService;
+            //Bookings = bookingService.GetAllBookings();
+        }
         public void OnGet()
         {
         }
